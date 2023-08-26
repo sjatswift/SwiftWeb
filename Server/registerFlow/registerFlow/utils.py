60654e83716b44080733fdf947192316f05192d3
sjatswift <shivam@swiftride.in>
2023-08-26 12:44:12 +0530

    deelted server

--- Server/registerFlow/registerFlow/utils.py
+++
@@ -1,7 +0,0 @@
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-
-class CustomUserObtainPairSerializer(TokenObtainPairSerializer):
-    def get_token(cls,user):
-        token = super().get_token(user)
-        
-        return token
--- Server/registerFlow/rootApp/admin.py
+++
@@ -1,3 +0,0 @@
-from django.contrib import admin
-from .models import SwiftUser
-# Register your models here.
